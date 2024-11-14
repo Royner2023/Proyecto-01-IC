1767d54d6cf5e4617a3e9ae65dbacd4e13193d18
Jherson Kelvin <diazbravojhersonkelvin@gmail.com>
2024-11-14 12:48:22 -0500

    Formulario encargado Jherson

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Mi nombre es Royner");
 Console.WriteLine("Este es otro cambio en git");
 Console.WriteLine("Ingresa un nombre");
 string nom = Console.ReadLine();
+
+Console.WriteLine("Creacion de Formulario encargado Jherson Kelvin");
